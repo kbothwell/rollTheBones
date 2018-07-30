f6e14114ec93ee05b3dd8ac54ddc38f62e724a0f
Korvin <kbothwell@gmail.com>
2018-07-30 09:55:28 -0400

    Initial Commit

---
+++ rollTheBones/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rollTheBones
+//
+//  Created by Korvin Bothwell on 7/30/18.
+//  Copyright © 2018 Korvin Bothwell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
